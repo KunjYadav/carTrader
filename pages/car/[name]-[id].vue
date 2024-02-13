103d54dbdcd22a4b844dc89705da77c8c4848cf6
DKillia <kunju.yadav555@gmail.com>
2024-02-14 01:07:15 +0530

    first commit

---
+++ pages/car/[name]-[id].vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <CarDetailHero :car="car" />
+    <CarDetailAttributes :features="car.features" />
+    <CarDetailDescription :description="car.description" />
+    <CarDetailContact />
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+
+const { data: car } = await useFetchCar(route.params.id);
+
+const { toTitleCase } = useUtilities();
+
+useHead({
+  title: toTitleCase(route.params.name),
+});
+
+if (!car.value) {
+  throw createError({
+    statusCode: 404,
+    message: `Car with Id of ${route.params.id} does not exist`,
+  });
+}
+
+definePageMeta({
+  layout: "custom",
+});
+</script>
